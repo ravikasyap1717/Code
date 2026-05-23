5db1319a40b2cfef41ca9dee99735eb1703aae39
ravikasyap1717 <ravikasyap1717@yahoo.com>
2026-05-23 12:10:35 +0530

    Create own_str.c

---
+++ own_str.c
@@ -0,0 +1,103 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<stdlib.h>
+//own strcpy
+
+char* strcpy_own(char *dest,char *src)
+{
+    char *start=dest;
+    int len=0;
+    while(*src!='\0')
+    {
+        *dest=*src;
+        dest++;
+        src++;
+        len++;
+    }
+    
+    *dest = '\0';
+    
+    return start;
+    
+}
+
+//concat 2 string
+char *concat_str(char *dest, char *src)
+{
+    char *start=dest;
+    while(*dest!='\0')
+    {
+        dest++;
+    }
+    while(*src !='\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+    
+    return start;
+}
+
+char *memcpy_own(char *dest, char *src, int numofBytes)
+{
+    char *start = dest;
+    for(int i=0;i<numofBytes;i++)
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    
+    //*dest ='\0';
+    return start;
+    
+}
+
+int strcmp_own(const char*str1, const char* str2)
+{
+    while((*str1!='\0' ) || (*str2 != '\0'))
+    {
+       if(*str1 == *str2)
+       {
+           str1++;
+           str2++;
+       }
+       else
+        return (*str1 - *str2);
+    }
+    
+    return 0;
+     
+}
+
+int main()
+{
+    printf("Hello World\n");
+    
+    char *name="Ravi Kumar";
+    char *S1;
+    S1 = (char*)malloc(sizeof(char) *20);
+
+    strcpy_own(S1,name);
+    printf("copy: %s\n",S1);
+    concat_str(S1,"Annu");
+    printf("Concat: %s\n",S1);
+    
+    char src[15]="ABCD 4&3";
+   src[8] ='\0';
+    src[9]='A';
+    memcpy_own(S1,src,15);
+    printf("memcpy :%s\n",S1);
+    
+    printf("strcmp :%d \n",strcmp_own("ACD","ABD"));
+    return 0;
+}
